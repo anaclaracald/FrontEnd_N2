110dc5e1aaa77b09a4b44c79f424ec5b2ba580cf
Ana Clara <ana05clara24@gmail.com>
2024-11-02 16:32:42 -0300

    Organização das pastas e arquivos, iniciei página do slideshow com bootstrap e inseri dois vídeos

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const balletCarousel = new bootstrap.Carousel('#balletCarousel', {
+        interval: 5000, // Change slide every 5 seconds
+        wrap: true      // Loop slides indefinitely
+    });
+});
